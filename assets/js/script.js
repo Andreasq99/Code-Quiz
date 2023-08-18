f8193c3942dba8a05e02ed0108cd03ac6da1e56e
LuminiferousGiraffe <andreas.quist99@gmail.com>
2023-08-17 23:51:51 -0400

    Added JavaScript for start of game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,83 @@
 var startBtn = document.querySelector(".start-button");
-var resetBtn = document.querySelector(".reset-button");
-var wScore = document.querySelector(".win");
-var lScore = document.querySelector(".lose");
-console.log(startBtn);
-console.log(resetBtn);
+var timer = document.querySelector(".timer-count");
+var qWindow = document.querySelector("#quiz-game");
 
 var wl = [0,0];
+var qIndex = 0;
 console.log(wl.length);
 console.log(wl[1]);
+console.log(timer.hasChildNodes());
+console.log(qWindow.hasChildNodes());
 
 var quizQs = [{
     question: "Which is not a common data type?",
-    answers: ["Boolean", "String", "Number", "Byte"]
+    answers: ["Boolean", "String", "Number", "Byte"],
+    ans: 3
 },
 {
     question: "The elements of an array are distinguished by which character?",
-    answers: ["Semicolon", "Colon", "Comma", "Period"]
+    answers: ["Semicolon", "Colon", "Comma", "Period"],
+    ans: 2
 },
 {
-    question: "",
+    question: "How do you reference an element in JavaScript with id 'hero'?",
+    answers: ["document.getElementById('.hero')", "document.getElementById('#hero')", "document.getElementById(#hero)", "document.getElementById(.hero)"],
+    ans: 1
+},
+{
+    question: "What does DOM stand for?",
+    answers: ["Document Object Model", "Document On My screen", "Department of Management", "Document Operations Manager"],
+    ans: 0
 }
 ];
 
-resetBtn.addEventListener("click", resetScore);
+window.alert("Hi! I was not able to complete this assignment on time, I asked for an extension though. Sorry!");
 
-function resetScore(){
-    console.log("reset score!");
-    wl = [0,0];
-    renderScore();
+function shufflerArr(arr){
+    var sampArr = arr;
+    var retArr = [];
+    var length = arr.length;
+    for(i=0;i<length;i++){
+        var index = Math.floor(Math.random()*sampArr.length);
+        retArr.push(sampArr[index]);
+        sampArr.splice(index,1);
+    }
+    return retArr;
 }
 
-function renderScore(){
-    wScore.textContent = wl[0];
-    lScore.textContent = wl[1];
+startBtn.addEventListener("click", startGame);
+
+function startGame(){
+    quizQs = shufflerArr(quizQs);
+    runTimer();
+    console.log(quizQs);
+    console.log(quizQs[0]);
+    console.log(quizQs[0].question);
+    renderQuestion(quizQs[0]);
 }
 
-renderScore();
+function runTimer(){
+    var timeLeft = 75;
+    var clock = setInterval(function(){
+        timeLeft--;
+        timer.textContent = timeLeft;
+        if(timeLeft === 0){
+            endGame(0);
+            clearInterval(clock);
+        }
+    },1000);
+}
+
+function renderQuestion(q){
+    while(qWindow.hasChildNodes()){
+        qWindow.removeChild(qWindow.firstChild);
+    }
+    var qQuestion = document.createElement("div");
+    qQuestion.innerHTML = q.question;
+    qWindow.appendChild(qQuestion);
+}
+
+function endGame(score){
+    console.log(score);
+    qIndex = 0;
+}
