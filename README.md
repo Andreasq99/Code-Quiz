0832f778f575adf173e0cc9375407317eb792f17
LuminiferousGiraffe <andreas.quist99@gmail.com>
2023-08-19 20:37:34 -0400

    Coded Leaderboard page

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Code-Quiz
 A quiz for testing your coding knowledge! The fourth assignment in my coding bootcamp.
+
+
+
+# Credits
+    I used a ton of Mozilla, Github, and W3 resources to learn how to code this. The code for sorting the leaderboard is based on this article from geeksforgeeks.org: [geeksforgeeks.org](https://www.geeksforgeeks.org/bubble-sort/)
--- assets/js/hs-script.js
+++ assets/js/hs-script.js
@@ -2,34 +2,187 @@ var startBtn = document.querySelector(".start-button");
 var yourScore = document.querySelector(".your-score");
 var qWindow = document.querySelector("#quiz-game");
 var scoreLink = document.querySelector("#score-link");
+var lboard = document.querySelector("#lb-list");
 
 var score = localStorage.getItem("score");
 yourScore.innerHTML = score;
 
+var lboardScores = [70, 45, 30, 20, score];
+var lboardNames = ["AQ", "BN", "HO", "GT", ""];
+var scoreIndex = 5;
+var lboardList = [];
 
-var lboard = [];
-var lboardNames = [];
+var initialsIndex = 3;
+var initials = ["",""];
+console.log(initials);
+
+/* localStorage testing
+localStorage.setItem("array",JSON.stringify([1,2,3,4,5]));
+var lsArr = JSON.parse(localStorage.getItem("array"));
+lsArr.push(6);
+localStorage.setItem("array",JSON.stringify(lsArr));
+console.log(localStorage.getItem("array"));
+*/
+
+document.addEventListener("keydown", function(event){
+    console.log(event.key);
+    console.log(initialsIndex);
+    if(initialsIndex<2){
+        initials[initialsIndex] = event.key.toUpperCase();
+        initialsIndex++;
+        console.log(initials);
+        console.log(initialsIndex);
+    }
+});
+
+refreshLboard();
 
 function refreshLboard(){
+    if(localStorage.getItem("lbs")===null){
+        localStorage.setItem("lbs",JSON.stringify(lboardScores));
+    } else {
+        lboardScores = JSON.parse(localStorage.getItem("lbs"));
+        lboardScores.push(score);
+    }
+    if(localStorage.getItem("lbn")===null){
+        localStorage.setItem("lbn", JSON.stringify(lboardNames));
+    } else {
+        lboardNames = JSON.parse(localStorage.getItem("lbn"));
+        lboardNames.push("");
+    }
+    console.log(lboardScores);
+    console.log(lboardNames);
+
+    lboardSort();
+    renderLboard();
+
+
+    
+/*  old, bad code  
     if (localStorage.getItem("lb1")=== null){
         localStorage.setItem("lb1", score);
-        lboard.push(score);
+        lboardScores.push(score);
     } else if (localStorage.getItem("lb2")=== null){
         localStorage.setItem("lb2", score);
-        lboard.push(score);
+        lboardScores.push(score);
     } else if (localStorage.getItem("lb3")=== null){
         localStorage.setItem("lb3", score);
-        lboard.push(score);
+        lboardScores.push(score);
     } else if (localStorage.getItem("lb4")=== null){
         localStorage.setItem("lb4", score);
-        lboard.push(score);
+        lboardScores.push(score);
     } else if (localStorage.getItem("lb5")=== null){
         localStorage.setItem("lb5", score);
-        lboard.push(score);
+        lboardScores.push(score);
     } else {
-        
+        lboardScores.push(lb1);
+        lboardScores.push(lb2);
+        lboardScores.push(lb3);
+        lboardScores.push(lb4);
+        lboardScores.push(lb5);
+        for (i=0;i<5;i++){
+            if (score >= lboardScores[i]){
+                newScoreIndex = i;
+            }
+        }
+        console.log(newScoreIndex);
+        if (newScoreIndex<lboardScores.length){
+            lboardScores.splice(newScoreIndex, 0, score);
+            lboardScores.
+        }
+    }
+*/
+}
+
+function lboardSort(){
+    console.log("lboardSort");
+    for(i=0;i<lboardScores.length-1;i++){
+        for(j=0;j<lboardScores.length-i-1;j++){
+            if(lboardScores[j]<lboardScores[j+1]){
+                tempScore = lboardScores[j];
+                lboardScores[j] = lboardScores[j+1];
+                lboardScores[j+1] = tempScore;
+
+                tempName = lboardNames[j];
+                lboardNames[j] = lboardNames[j+1];
+                lboardNames[j+1] = tempName;
+
+                console.log(lboardScores);
+                console.log(lboardNames);
+            }
+        }
+
+    }
+    scoreIndex = lboardNames.indexOf("");
+    console.log(scoreIndex);
+    if(scoreIndex<5){
+        initialsIndex = 0;
+        lboardNames[lboardNames.indexOf("")] = "N/A";
+    }
+    console.log(scoreIndex);
+    if(lboardScores.length>5){
+        lboardScores.splice(5,lboardScores.length-5);
+    }
+    if(lboardNames.length>5){
+        lboardNames.splice(5,lboardNames.length-5);
+    }
+
+    localStorage.setItem("lbs", JSON.stringify(lboardScores));
+    localStorage.setItem("lbn", JSON.stringify(lboardNames));
+}
+
+function renderLboard(){
+
+    console.log("renderLboard");
+    for(i=0; i<lboardScores.length;i++){
+        lboardList.push(document.createElement("li"));
+        var listName = document.createElement("span");
+        listName.textContent = lboardNames[i];
+        var listScore = document.createElement("span");
+        listScore.textContent = lboardScores[i];
+        lboard.appendChild(lboardList[i]);
+        lboardList[i].appendChild(listName);
+        lboardList[i].appendChild(listScore);
+    }
+    console.log(initialsIndex);
+    if(initialsIndex===0){
+        initialPrompt();
+    } else {
+        var lossMessage = document.createElement("div");
+        lossMessage.textContent = "You did not make the leaderboard!";
+        var scoreSheet = document.querySelector("#score-sheet");
+        scoreSheet.appendChild(lossMessage);
+        console.log("loss");
     }
 }
+
+function initialPrompt(){
+    lboardList[scoreIndex].firstChild.textContent = " ";
+    console.log("initialPrompt");
+    var tickToggler = true;
+    var ticker = setInterval(function(){
+        if(initialsIndex===0){
+            if(tickToggler){
+                lboardList[scoreIndex].firstChild.textContent = "_";
+            } else {
+                lboardList[scoreIndex].firstChild.textContent = " ";
+            }
+        } else if(initialsIndex===1){
+            if(tickToggler){
+                lboardList[scoreIndex].firstChild.textContent = initials[0] + "_";
+            } else {
+                lboardList[scoreIndex].firstChild.textContent = initials[0] + " ";
+            }
+        } else {
+            lboardList[scoreIndex].firstChild.textContent = initials[0] + initials[1];
+            clearInterval(ticker);
+            lboardNames[scoreIndex] = initials[0] + initials[1];
+            localStorage.setItem("lbn",JSON.stringify(lboardNames));
+        }
+        tickToggler = !tickToggler;
+    }, 750);
+}
+
 
 function shufflerArr(arr){
     var sampArr = arr;
@@ -41,74 +194,4 @@ function shufflerArr(arr){
         sampArr.splice(index,1);
     }
     return retArr;
-}
-
-
-function runTimer(){
-    timeLeft = 75;
-    clock = setInterval(function(){
-        timeLeft--;
-        timer.textContent = timeLeft;
-        if(timeLeft === 0){
-            endGame(0);
-            clearInterval(clock);
-        }
-    },1000);
-}
-
-function renderQuestion(q){
-    while(qWindow.hasChildNodes()){
-        qWindow.removeChild(qWindow.firstChild);
-    }
-    var qQuestion = document.createElement("div");
-    var qAnswers = document.createElement("ul");
-    qQuestion.innerHTML = q.question;
-    qWindow.appendChild(qQuestion);
-    qWindow.appendChild(qAnswers);
-    var ans = [];
-    for(i=0;i<q.answers.length; i++){
-        ans.push(document.createElement("li"));
-        console.log(q.answers)
-        ans[i].innerHTML = q.answers[i];
-        qAnswers.appendChild(ans[i]);
-        if (i===q.ans){
-            ans[i].addEventListener("click", correctAnswer);
-        } else {
-            ans[i].addEventListener("click", incorrectAnswer);
-        }
-    }
-}
-
-function incorrectAnswer(){
-    qIndex++;
-    timeLeft-=20;
-    if(qIndex === quizQs.length){
-        endGame();
-    } else {
-        renderQuestion(quizQs[qIndex]);
-    }
-}
-
-
-function correctAnswer(){
-    qIndex++;
-    if(qIndex === quizQs.length){
-        endGame();
-    } else {
-        renderQuestion(quizQs[qIndex]);
-    }
-}
-
-function endGame(){
-    console.log(timeLeft);
-    localStorage.setItem("score", timeLeft);
-    qIndex = 0;
-    clearInterval(clock);
-    while(qWindow.hasChildNodes()){
-        qWindow.removeChild(qWindow.firstChild);
-    }
-    var startText = document.createElement("div");
-    startText.innerHTML = "Click Start to begin!";
-    qWindow.appendChild(startText);
-    scoreLink.style.display = "inline";
 }
